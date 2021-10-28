c5f11d8b3f7f101cede5435614bb8086eab6798a
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-28 19:45:46 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const apiRoutes = require("./routes/api.js");
 
-const PORT = process.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
@@ -16,14 +16,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-const mongoUri = process.env.MONGODB_URI || "mongodb://localhost/Budget-Tracker";
-mongoose.connect(mongoUri, {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
+  useFindAndModify: false
 });
 
-app.use(apiRoutes);
+// routes
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
