d6f618bc0120a116c1a1a1def4932f8348826f4d
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-29 11:51:54 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 const express = require("express");
-const htmlRoutes = require("./routes/htmlRoutes");
-const apiRoutes = require("./routes/apiRoutes");
-const app = express();
-const PORT = process.env.PORT || 3001;
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
 
-app.use(express.json());
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(express.static("public"));
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`Listening on PORT ${PORT}`);
+  console.log(`App running on port ${PORT}!`);
 });
