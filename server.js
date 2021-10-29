205cade1a2979d626911ae86684d4e05a44ec99c
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-29 10:59:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = (process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
-const app = express());
+const app = express();
 
 app.use(logger("dev"));
 
