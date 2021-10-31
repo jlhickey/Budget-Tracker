90fab9902d3496cb6f2fb7a9e9dad9054426e391
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-31 10:29:11 -0400

    Update server.js

--- server.js
+++ server.js
@@ -16,9 +16,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+// Connect to mongoose
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
