a399e7deae4b6df58473691ffa05da51801acbd1
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-31 10:23:37 -0400

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,59 @@
+// Set constants for the cache name
+const APP_PREFIX = 'BudgetTrack-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+// Files that need to be cached are index.html and the css styles file
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./css/styles.css"
+];
+
+// Install the service worker
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+})
+
+// Activate the service worker
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeeplist.push(CACHE_NAME);
+
+            return Promise.all(
+                keyList.map(function (key, i) {
+                    if (cacheKeeplist.indexOf(key) === -1) {
+                        console.log('deleting cache : ' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// When there is a fetch request, check to see if cached
+// If cached, return the cached version
+// If not cached, fetch the remote webpage
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else {
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+        })
+    )
+})
