4452038e69116f683a57eb2fe13d2ffd40b223c3
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-31 10:26:14 -0400

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
+// Route for POST request to transaction
 router.post("/api/transaction", ({body}, res) => {
   Transaction.create(body)
     .then(dbTransaction => {
@@ -11,6 +12,7 @@ router.post("/api/transaction", ({body}, res) => {
     });
 });
 
+// Route for a POST request for a bulk of transactions
 router.post("/api/transaction/bulk", ({body}, res) => {
   Transaction.insertMany(body)
     .then(dbTransaction => {
@@ -21,6 +23,7 @@ router.post("/api/transaction/bulk", ({body}, res) => {
     });
 });
 
+// Route for a GET request for transactions
 router.get("/api/transaction", (req, res) => {
   Transaction.find({}).sort({date: -1})
     .then(dbTransaction => {
